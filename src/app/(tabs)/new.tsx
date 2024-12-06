819938e8ef042356c957d02ea5881dc308ed0804
steve <coopersilvanus27@gmail.com>
2024-12-06 22:46:21 +0000

    image picker added

--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -6,10 +6,35 @@ import {
   Pressable,
   ScrollView,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
 
 const CreateScreen = () => {
   const [caption, setCaption] = useState("Hello world. How are you all doing?");
+  const [image, setImage] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!image) {
+      pickImage();
+    }
+  }, [image]);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <ScrollView>
       <View className="m-4 gap-4 h-full">
@@ -17,12 +42,14 @@ const CreateScreen = () => {
         <View className="p-3 items-center">
           <Image
             source={{
-              uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+              uri: image
+                ? image
+                : "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
             }}
             className="w-80 aspect-[3/4] rounded-lg bg-slate-300"
           />
 
-          <Text onPress={() => {}} className="text-blue-500 font-semibold m-5">
+          <Text onPress={pickImage} className="text-blue-500 font-semibold m-5">
             Change
           </Text>
         </View>
